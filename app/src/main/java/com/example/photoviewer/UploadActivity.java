784e6b4d2782baad2a4c9f194ec7ff8b96d4a49e
phillip9807@gmail.com <phillip9807@gmail.com>
2021-02-04 22:21:38 +0900

    upload image

--- app/src/main/java/com/example/photoviewer/UploadActivity.java
+++ app/src/main/java/com/example/photoviewer/UploadActivity.java
@@ -1,17 +1,95 @@
 package com.example.photoviewer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class UploadActivity extends AppCompatActivity {
+
+    ImageView mImageView;
+    Button mChooseBtn;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        // VIEWS
+        mImageView = findViewById(R.id.imageView);
+        mChooseBtn = findViewById(R.id.UploadBtn);
+
+        // Handle Button Click
+        mChooseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                // Check Runtime Permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        // Permission NOT granted, request it
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        // Show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    }
+                    else {
+                        // Permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+                else {
+                    // System OS is less than Marshmallow
+                    pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        // Intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    // Handle the result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted
+                    pickImageFromGallery();
+                }
+                else {
+                    // Permission was denied
+                    Toast.makeText(this, "Permission was denied...!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    // Handle the result of picked image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // Set image to image view
+            mImageView.setImageURI(data.getData());
+        }
     }
 
     public void BackToMain (View v) {
