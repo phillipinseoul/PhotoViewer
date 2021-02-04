242c936471b0c382f8890e5ff3c964565341f983
phillip9807@gmail.com <phillip9807@gmail.com>
2021-02-04 13:03:04 +0900

    add upload acitivity

--- app/src/main/java/com/example/photoviewer/MainActivity.java
+++ app/src/main/java/com/example/photoviewer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.photoviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     ImageView img;
     Button b;
     int flag = 0;
+
+    public void UploadActivity (View v) {
+        Intent i = new Intent(this, UploadActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/photoviewer/UploadActivity.java
@@ -0,0 +1,18 @@
+package com.example.photoviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+
+
+}
